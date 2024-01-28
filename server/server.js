dc4099ce0fa036897c0779645d1cf8f318cdc4fb
VinayakSingh2001 <vinayakthakur843@gmail.com>
2024-01-29 03:08:33 +0530

    added packages and socketconnection in client

--- server/server.js
+++ server/server.js
@@ -1,1 +1,63 @@
 // its namely the entry point for our cluster
+const cluster = require("cluster");
+const http = require("http");
+const { Server } = require("socket.io");
+const numCPUs = require("os").cpus().length;
+const { setupMaster, setupWorker } = require("@socket.io/sticky");
+const { createAdapter, setupPrimary } = require("@socket.io/cluster-adapter"); // make is it so that primary node can emit to everybody
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  const httpServer = http.createServer();
+
+  // setup sticky sessions
+  setupMaster(httpServer, {
+    loadBalancingMethod: "least-connection",
+  });
+
+  // setup connections between the workers
+  setupPrimary();
+
+  // needed for packets containing buffers (you can ignore it if you only send plaintext objects)
+  // Node.js < 16.0.0
+  //   cluster.setupMaster({
+  //     serialization: "advanced",
+  //   });
+  // Node.js > 16.0.0
+  cluster.setupPrimary({
+    serialization: "advanced",
+  });
+
+  httpServer.listen(3000); // internet facing
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker) => {
+    console.log(`Worker ${worker.process.pid} died`);
+    cluster.fork();
+  });
+} else {
+  console.log(`Worker ${process.pid} started`);
+
+  const httpServer = http.createServer();
+  const io = new Server(httpServer, {
+    cors: {
+      origin: "http://localhost:3001",
+      credentials: true,
+    },
+  });
+
+  // use the cluster adapter
+  io.adapter(createAdapter()); //change from the default adapter
+
+  // setup connection with the primary process
+  setupWorker(io);
+
+  io.on("connection", (socket) => {
+    console.log(`someone connected on worker ${process.pid}`);
+    socket.emit("welcome", "welcome to our cluster driven socket.io server");
+  });
+}
