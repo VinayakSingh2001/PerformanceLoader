d6df6cb02ce4d9c311d2332ae326db18e960233b
VinayakSingh2001 <vinayakthakur843@gmail.com>
2024-02-03 00:38:18 +0530

    added cpu widget

--- react-client/src/perfDataComponets/Cpu.js
+++ react-client/src/perfDataComponets/Cpu.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
+import drawCircle from "../utilities/canvasLoadAnimation";
 
-const Cpu = () => {
-  return <div className="cpu col-3">cpu</div>;
+const Cpu = ({ data }) => {
+  const canvasEl = useRef();
+  drawCircle(canvasEl.current, data.cpuLoad);
+  return (
+    <div className="cpu col-3">
+      <h3>CPU Load</h3>
+      <div className="canvas-wrapper">
+        <canvas ref={canvasEl} className="" width="200" height="200"></canvas>
+        <div className="cpu-text">{data.cpuLoad}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Cpu;
